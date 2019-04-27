d249ba011a8cc8cb6ea35444fad7098e77c4d80d
Artur Gawlik <arturgawlik@MacBook-Air-Artur.local>
2019-04-28 00:55:11 +0200

    add shell guard

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,16 +2,21 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { ShellComponent } from './components/shell/shell.component';
 import { LoginComponent } from './components/login/login.component';
+import { AuthGuard } from './guards/auth/auth.guard';
+import { ShellGuard } from './guards/shell/shell.guard';
 
 const routes: Routes = [
   {
     path: '',
     component: ShellComponent, 
-    children: []
+    children: [],
+    canActivate: [ShellGuard],
+    canActivateChild: [ShellGuard]
   },
   {
     path: 'login',
-    component: LoginComponent
+    component: LoginComponent,
+    canActivate: [AuthGuard]
   }
 ];
 
---
+++ src/app/guards/shell/shell.guard.ts
@@ -0,0 +1,29 @@
+import { Injectable } from '@angular/core';
+import { CanActivate, ActivatedRouteSnapshot, RouterStateSnapshot, UrlTree, Router } from '@angular/router';
+import { Observable } from 'rxjs';
+import { UserService } from 'src/app/services/user/user.service';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class ShellGuard implements CanActivate {
+  
+  constructor(private userService: UserService, private router: Router) {
+  }
+  
+  canActivate(next: ActivatedRouteSnapshot, state: RouterStateSnapshot): Promise<boolean > {
+    return this.userService.getCurrentUser()
+      .then(usr => {
+        if (usr) {
+          return true;
+        } else {
+          this.router.navigate(['/login']);
+          return false;
+        }
+      })
+      .catch(err => {
+        return false;
+      })
+  }
+  
+}
