bfc9b9de3fce2ed5062e1d8321b2aaa36c2dceff
Artur Gawlik <arturgawlik@MacBook-Air-Artur.local>
2019-04-27 22:01:23 +0200

    add facebook/google auth methods

--- src/app/services/auth/auth.service.ts
+++ src/app/services/auth/auth.service.ts
@@ -1,9 +1,33 @@
 import { Injectable } from '@angular/core';
+import { AngularFireAuth } from '@angular/fire/auth';
+import * as firebase from 'firebase';
 
 @Injectable({
   providedIn: 'root'
 })
 export class AuthService {
 
-  constructor() { }
+  constructor(private afAuth: AngularFireAuth) {
+  }
+
+  doFacebookLogin() {
+    return new Promise<any>((resolve, reject) => {
+      let provider = new firebase.auth.FacebookAuthProvider();
+      this.afAuth.auth
+      .signInWithPopup(provider)
+      .then(res => resolve(res), err => reject(err));
+    });
+  }
+
+  doLoginWithGoogle() {
+    return new Promise<any>((resolve, reject) =>{
+      let provider = new firebase.auth.GoogleAuthProvider();
+      provider.addScope('profile');
+      provider.addScope('email');
+      this.afAuth.auth
+      .signInWithPopup(provider)
+      .then(res => resolve(res), err => reject(err));
+    })
+  }
+
 }
