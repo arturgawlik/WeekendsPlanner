39dac9152dec0e0bd26b0bf696d9e16c48cf102a
Artur Gawlik <arturgawlik@MacBook-Air-Artur.local>
2019-04-27 20:24:33 +0200

    base routing setup

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,19 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { ShellComponent } from './src/app/components/shell/shell.component';
+import { LoginComponent } from './src/app/components/login/login.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    component: ShellComponent, 
+    children: []
+  },
+  {
+    path: 'login',
+    component: LoginComponent
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
