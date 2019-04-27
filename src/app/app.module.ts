d0ce52eb11fa24545e8b06ebe97ee7cada03ca4c
Artur Gawlik <arturgawlik@MacBook-Air-Artur.local>
2019-04-27 20:54:31 +0200

    import and setup angular fire modules

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,10 +1,16 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
+import { AngularFireModule } from '@angular/fire';
+import { AngularFirestoreModule } from '@angular/fire/firestore';
+import { AngularFireAuthModule } from '@angular/fire/auth';
+import { environment } from '../environments/environment';
+
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { ShellComponent } from './src/app/components/shell/shell.component';
 import { LoginComponent } from './src/app/components/login/login.component';
+
 
 @NgModule({
   declarations: [
@@ -14,7 +20,10 @@ import { LoginComponent } from './src/app/components/login/login.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    AngularFireModule.initializeApp(environment.firebase),
+    AngularFirestoreModule,
+    AngularFireAuthModule
   ],
   providers: [],
   bootstrap: [AppComponent]
